96a59410e4e190b293ec272447134bf9fdc76890
Mateus Andrade <mateusdoandrade@gmail.com>
2019-11-27 00:08:10 -0300

    Completing task - Get rid of the BaseActivity

--- app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
+++ app/src/main/java/com/arctouch/codechallenge/home/HomeActivity.kt
@@ -2,13 +2,13 @@ package com.arctouch.codechallenge.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.arctouch.codechallenge.R
-import com.arctouch.codechallenge.base.BaseActivity
 import kotlinx.android.synthetic.main.home_activity.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class HomeActivity : BaseActivity() {
+class HomeActivity : AppCompatActivity() {
 
     private val homeActivityViewModel: HomeActivityViewModel by viewModel()
 
